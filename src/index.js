c98e9921df41ad21dee7b26ddb5078b28db1643b
ympbyc <ympbyc@gmail.com>
2013-01-28 12:39:50 +0900

    compiler CLI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 (function () {
   'use strict';
 
-  var fs, excam;
+  var fs, excam, beautify;
 
   fs = require('fs');
   excam = require('./expensivecamera');
+  beautify = require('../lib/beautify');
 
-  if (process.argv[1]) {
-    fs.readFile(process.argv[1], 'utf8', function (err, src) {
+  if (process.argv[2]) {
+    fs.readFile(process.argv[2], 'utf8', function (err, src) {
       if (err) throw err;
-      console.log(excam.parse(src));
+      console.log(
+        beautify.js_beautify(excam.parse(src), {
+          indent_size: 2
+        , indent_char: ' '
+        , jslint_happy: true
+        }));
     });
+  }
+
+  else {
+    console.log('REPL is comming soon');
   }
 }());
